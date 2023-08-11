62d354f4efc83774bb2d1400fa085d2c1daf1726
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-08-12 01:02:46 +0200

    next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stat" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
