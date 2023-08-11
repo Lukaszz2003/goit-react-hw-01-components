2146aa92c2f3d50cdd4be85a7ed553e8ec8653d1
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-08-11 23:09:16 +0200

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        padding: 50,
+        height: 'auto',
         display: 'flex',
+        flexDirection: 'column',
+        gap: 100,
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 24,
+        color: '#010101',
+        backgroundColor: 'lightgray',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
